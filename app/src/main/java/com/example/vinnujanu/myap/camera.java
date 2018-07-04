19aa51591e919df5c660f59ef04779dbbae248b5
sasankchindirala <sasankchindirala@gmail.com>
2018-07-05 00:38:37 +0530

    latest commit

--- app/src/main/java/com/example/vinnujanu/myap/camera.java
+++ app/src/main/java/com/example/vinnujanu/myap/camera.java
@@ -2,6 +2,8 @@ package com.example.vinnujanu.myap;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +11,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class camera extends AppCompatActivity {
     Button btnCamera;
@@ -36,7 +46,7 @@ public class camera extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,0);
+                startActivityForResult(intent,20);
 
             }
         });
@@ -46,17 +56,64 @@ public class camera extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if(x==0) {
             super.onActivityResult(requestCode, resultCode, data);
-            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(bitmap);
-            x = 1;
+            if( resultCode == RESULT_OK){
+                Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+
+                String partFilename = currentDateFormat();
+                storeCameraPhotoInSDCard(bitmap, partFilename);
+
+                // display the image from SD Card to ImageView Control
+                String storeFilename = "photo_" + partFilename + ".jpg";
+                Bitmap mBitmap = getImageFileFromSDCard(storeFilename);
+                Bitmap bitma=(Bitmap)data.getExtras().get("data");
+                imageView.setImageBitmap(bitma);
+                x=1;
+            }
         }
         else{
-            super.onActivityResult(requestCode, resultCode, data);
-            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-            imageVieww2.setImageBitmap(bitmap);
+            Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+
+            String partFilename = currentDateFormat();
+            storeCameraPhotoInSDCard(bitmap, partFilename);
+
+            // display the image from SD Card to ImageView Control
+            String storeFilename = "photo_" + partFilename + ".jpg";
+            Bitmap mBitmap = getImageFileFromSDCard(storeFilename);
+            Bitmap bitma=(Bitmap)data.getExtras().get("data");
+            imageVieww2.setImageBitmap(bitma);
+
             x=0;
 
         }
 
+    }
+    private String currentDateFormat(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HH_mm_ss");
+        String  currentTimeStamp = dateFormat.format(new Date());
+        return currentTimeStamp;
+    }
+    private void storeCameraPhotoInSDCard(Bitmap bitmap, String currentDate){
+        File outputFile = new File(Environment.getExternalStorageDirectory(), "photo_" + currentDate + ".jpg");
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private Bitmap getImageFileFromSDCard(String filename){
+        Bitmap bitmap = null;
+        File imageFile = new File(Environment.getExternalStorageDirectory() + filename);
+        try {
+            FileInputStream fis = new FileInputStream(imageFile);
+            bitmap = BitmapFactory.decodeStream(fis);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
     }
 }
